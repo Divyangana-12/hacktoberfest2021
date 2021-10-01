a8e5889bf873118b71131363443df221567b9354
vmeet95 <91734696+vmeet95@users.noreply.github.com>
2021-10-01 17:33:14 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -3,13 +3,21 @@ int main()
 {
     int n,i;
     int arr[100];
+    
+    printf("Enter Value of Array Elements : ");
     scanf("%d",&n);
-    for (i=0;i<n;i++){
+    
+    for (i=0;i<n;i++)
+    {
+        printf("arr[%d] : ",i);
         scanf("%d",&arr[i]);
     }
-    for (i=0;i<n+1;i++){
-        if (arr[i]>arr[i+1]){
-            printf("%d\n",arr[i]);
+    
+    for (i=0;i<n+1;i++)
+    {
+        if (arr[i]>arr[i+1])
+        {
+            printf("%d ",arr[i]);
         }
     }
     return 0;
