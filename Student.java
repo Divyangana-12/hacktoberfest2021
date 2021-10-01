9ff250dbd87140d7f1d3172a84e61bcad1ea24ae
RITIKA MUKHERJEE <60706878+RitikaMukherjee@users.noreply.github.com>
2021-10-01 21:21:10 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main(String[] args) {
+        for(int i =0;i<args.length;i++){
+            System.out.println(i+" Hello "+args[i]);
+        }
+    }
+}
