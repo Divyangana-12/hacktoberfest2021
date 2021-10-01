2e4ba4fe3250340f027b9d5f3fe65f683d353eac
RITIKA MUKHERJEE <60706878+RitikaMukherjee@users.noreply.github.com>
2021-10-01 21:21:41 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+public class Pattern {
+    public static void main(String[] args)  {
+        
+        
+        int i, j, row = 5;       
+        for (i=0; i<row; i++){
+            for (j=row-i; j>0; j--){  
+                System.out.print(" ");
+                
+            }  
+                for (j=0; j<=i; j++ ){   
+                    System.out.print("* ");   
+            }   
+        System.out.println();   
+        }   
+    }
+}
